e5fc6788943da08e6a26bd616514f74a0aa687db
Mmaaay <mazenyousef.my@gmail.com>
2025-01-17 16:06:10 +0200

    added email and better image ,cv

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,7 +4,7 @@ import { useSectionInView } from "@/lib/hooks";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { FaGithubSquare } from "react-icons/fa";
 import { HiDownload } from "react-icons/hi";
@@ -12,13 +12,15 @@ import { HiDownload } from "react-icons/hi";
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.75);
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <section
-      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      className="scroll-mt-[100rem] mb-28 sm:mb-0 max-w-[50rem] text-center"
       id="home"
       ref={ref}
     >
-      <div className="flex items-center justify-center">
+      <div className="flex justify-center items-center">
         <div className="relative">
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
@@ -35,23 +37,26 @@ export default function Intro() {
               height="192"
               quality="95"
               priority={true}
-              className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl hover:scale-[2.5] transition duration-500 cursor-pointer hover:z-[999] hover:relative"
+              className="hover:relative hover:z-[999] border-[0.35rem] border-white shadow-xl rounded-full w-24 h-24 transition duration-700 cursor-pointer hover:scale-[2.5] object-cover"
+              onClick={() => setIsExpanded(!isExpanded)}
             />
           </motion.div>
-          <motion.span
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 260,
-              duration: 0.7,
-              damping: 20,
-              delay: 0.1,
-            }}
-            className="absolute text-4xl bottom-0 right-0"
-          >
-            👋
-          </motion.span>
+          {!isExpanded && (
+            <motion.span
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 260,
+                duration: 0.7,
+                damping: 20,
+                delay: 0.1,
+              }}
+              className="right-0 bottom-0 absolute text-4xl"
+            >
+              👋
+            </motion.span>
+          )}
         </div>
       </div>
       <motion.h1
@@ -62,7 +67,7 @@ export default function Intro() {
           duration: 0.6,
           delay: 0.3,
         }}
-        className="mb-8 mt-4 px-4 text-lg font-medium !leading-[1.5] sm:text-xl"
+        className="mt-4 mb-8 px-4 font-medium text-lg sm:text-xl !leading-[1.5]"
       >
         Hello! I'm <span className="font-bold">Mazen</span>, a passionate Full
         Stack Developer and AI enthusiast. 🚀 With expertise in React, Next.js,
@@ -71,37 +76,37 @@ export default function Intro() {
       </motion.h1>
 
       <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-3 px-4 text-lg font-medium"
+        className="flex sm:flex-row flex-col justify-center items-center gap-3 px-4 font-medium text-lg"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ type: "tween", duration: 0.3, delay: 0.5 }}
       >
         <Link
           href="#contact"
-          className="group flex bg-gray-900 text-white px-7 py-3 items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          className="flex items-center gap-2 bg-gray-900 hover:bg-gray-950 px-7 py-3 rounded-full text-white transition hover:scale-110 focus:scale-110 active:scale-105 group outline-none"
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
           }}
         >
           Contact Me Here
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          <BsArrowRight className="opacity-70 transition group-hover:translate-x-1" />
         </Link>
         <a
           href="assets/Mazen's CV.pdf"
           download
-          className="group flex border border-black/10 bg-white text-black px-7 py-3 items-center gap-2 rounded-full cursor-pointer outline-none hover:text-gray-950  hover:scale-[1.15]  active:scale-105 transition dark:bg-white/10 dark:text-white"
+          className="flex items-center gap-2 bg-white dark:bg-white/10 px-7 py-3 border border-black/10 rounded-full text-black hover:text-gray-950 dark:text-white transition cursor-pointer group hover:scale-[1.15] active:scale-105 outline-none"
         >
           Download CV
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          <HiDownload className="opacity-60 transition group-hover:translate-y-1" />
         </a>
-<div className="flex flex-row gap-4">
+        <div className="flex flex-row gap-4">
           <a
             rel="noopener"
             title="Linkedin"
             href="https://www.linkedin.com/in/mazen-mohammedaay/"
             target="_blank"
-            className="group flex bg-white border border-black/10 text-gray-700 p-4 items-center gap-2 rounded-full cursor-pointer outline-none hover:text-gray-950 focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition  dark:text-white/60 dark:bg-white/10"
+            className="flex items-center gap-2 bg-white dark:bg-white/10 p-4 border border-black/10 rounded-full text-gray-700 hover:text-gray-950 dark:text-white/60 transition cursor-pointer hover:scale-[1.15] focus:scale-[1.15] active:scale-105 group outline-none"
           >
             <BsLinkedin />
           </a>
@@ -110,11 +115,11 @@ export default function Intro() {
             title="Github"
             href="https://github.com/Mmaaay"
             target="_blank"
-            className="group flex bg-white border border-black/10 text-gray-700 p-4 items-center gap-2 text-[1.35rem] rounded-full cursor-pointer outline-none hover:text-gray-950 focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition dark:text-white/60 dark:bg-white/10 "
+            className="flex items-center gap-2 bg-white dark:bg-white/10 p-4 border border-black/10 rounded-full text-[1.35rem] text-gray-700 hover:text-gray-950 dark:text-white/60 transition cursor-pointer focus:scale-[1.15] group hover:scale-[1.15] active:scale-105 outline-none"
           >
             <FaGithubSquare />
           </a>
-</div>
+        </div>
       </motion.div>
     </section>
   );
--- lib/data.ts
+++ lib/data.ts
@@ -4,7 +4,8 @@ import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import asl from "../public/assets/Capture.jpg";
 import Hotel from "../public/assets/Capture2.png";
-
+import main from "../public/assets/main.jpg";
+import rooms from "../public/assets/rooms.jpg";
 
 export const links = [
   {
@@ -37,8 +38,7 @@ export const experiencesData = [
   {
     title: "Majored in Data Science",
     location: "Alexandia , EG",
-    description:
-      "Switched majors to Data Science.",
+    description: "Switched majors to Data Science.",
     icon: React.createElement(LuGraduationCap),
     date: "2021",
   },
@@ -46,17 +46,25 @@ export const experiencesData = [
     title: "ML & DL",
     location: "Alexandia , EG",
     description:
-      "Developed couple of DL model of Computer Vision such as ASL detector, Fake Logo Detection",
+      "Using advanced Deep learning algortihms to develope a ASL detection model through camera.",
     icon: React.createElement(CgWorkAlt),
     date: "2021 - 2022",
   },
   {
-    title: "Full-Stack Developer",
+    title: "MERN Full Stack Developer",
     location: "Alexandia , EG",
     description:
-      "Started full stack development using MERN And Next.js Developing this portofolio and also a Booking.com Clone",
+      "Using MERN stack developed a full stack hotel booking website.",
     icon: React.createElement(FaReact),
-    date: "2023 - present",
+    date: "2021 - 2022",
+  },
+  {
+    title: "NextJs Developer",
+    location: "Alexandia , EG",
+    description:
+      "Using NextJs developed a fully functioning RTC app, a Reddit clone.",
+    icon: React.createElement(FaReact),
+    date: "2022 - 2024",
   },
 ] as const;
 
@@ -74,7 +82,21 @@ export const projectsData = [
       "Developed a computer vision model to recognize American Sign Language. I used Python, TensorFlow, YOLO.",
     tags: ["Python", "Machine Learning", "Deep Learning", "Computer Vision"],
     imageUrl: asl,
-  }
+  },
+  {
+    title: "Reddit Clone",
+    description:
+      "Using NextJs, developed a fully functioning Reddit clone with features like authentication, posting, commenting, upvoting, downvoting.",
+    tags: ["NextJs", "React", "TypeScript"],
+    imageUrl: main,
+  },
+  {
+    title: "Discord Clone",
+    description:
+      "Using NextJs, developed a fully functioning Discord clone with features like authentication, channels, voice chat, video chat.",
+    tags: ["NextJs", "React", "TypeScript", "SocketIo"],
+    imageUrl: rooms,
+  },
 ] as const;
 
 export const skillsData = [
